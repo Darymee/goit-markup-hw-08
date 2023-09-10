24b4af19dfc6101fc0df0cf285cf2764cdebca9d
Darymee <danieledaniele3005@gmail.com>
2023-09-10 13:41:52 +0200

    translation into English and small fixes

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,12 @@
     closeMenuBtn: document.querySelector(".menu-close-btn"),
     menu: document.querySelector(".mob-menu"),
     body: document.querySelector("body"),
+    contactsLink: document.querySelector(".mob-nav__contact"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.contactsLink.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
